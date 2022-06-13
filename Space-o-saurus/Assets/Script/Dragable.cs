ce935c80059d9ff6c64a6b2a8bdd05f3ca711bb7
Maximilien Galea <m.galea@student.isartdigital.com>
2022-06-13 15:12:49 +0200

    drag and drop system

--- Space-o-saurus/Assets/Script/Dragable.cs
+++ Space-o-saurus/Assets/Script/Dragable.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Dragable : MonoBehaviour
 {
+
     [SerializeField]
     private Collider coll = null;
+    [SerializeField]
+    private bool returnToBasePoint;
+    [SerializeField]
+    private float speedToReturn;
+    [SerializeField]
+    private DragData dragData;
+    public DragData DragData => dragData;
+
+    public bool Placed { get; protected set; } = false;
 
     private Vector3 startPos = new Vector3();
-    private bool holded = false;
+    private float distance;
+    private bool dragging;
+    private Vector3 newPos;
+    private bool draggable = true;
 
     private void Reset()
     {
@@ -22,14 +35,45 @@ public class Dragable : MonoBehaviour
         startPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        if (!draggable)
+            return;
+
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
     }
 
-    private void OnMouseDrag()
+    void OnMouseUp()
     {
-        //transform.position = 
+        dragging = false;
+        if (Placed)
+            transform.position = newPos;
     }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            rayPoint.z = startPos.z;
+            transform.position = rayPoint;
+
+        }
+        else
+        {
+            if (!Placed && returnToBasePoint)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPos, speedToReturn * Time.deltaTime);
+            }
+        }
+    }
+
+    public void PlaceToCenter(Vector3 point, bool placed)
+    {
+        newPos = point;
+        Placed = placed;
+    }
+
 }
